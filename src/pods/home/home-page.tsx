40087d4289280e6f20bb65461719a0d9be84246e
Salino3 <flaviobcn012@gmail.com>
2024-07-20 16:17:24 +0200

    add multilanguage

--- src/pods/home/home-page.tsx
+++ src/pods/home/home-page.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
-import "./home-page.styles.scss";
+import { useTranslation } from "react-i18next";
 import { ServicesApp } from "@/core";
+import "./home-page.styles.scss";
 
 export const HomePage: React.FC = () => {
   const { fetchPaginatedData } = ServicesApp();
+  const [t, i18n] = useTranslation("global");
+
+  //
   useEffect(() => {
     fetchPaginatedData(1, 5, {})
       .then((res) => {
@@ -15,7 +19,7 @@ export const HomePage: React.FC = () => {
   }, []);
   return (
     <div className="rootHomePage">
-      <h1 className="titleHomePage">Home</h1>
+      <h1 className="titleHomePage">{t("home.title")}</h1>
     </div>
   );
 };
