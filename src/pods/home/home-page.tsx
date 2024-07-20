d1582f3d8350c26e284baf5a4e8b04365e2af113
Salino3 <flaviobcn012@gmail.com>
2024-07-20 15:16:40 +0200

    add some class

--- src/pods/home/home-page.tsx
+++ src/pods/home/home-page.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./home-page.styles.scss";
 
 export const HomePage: React.FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="rootHonePage">
+      <h1 className="titleHomePage">Home</h1>
+    </div>
+  );
 };
