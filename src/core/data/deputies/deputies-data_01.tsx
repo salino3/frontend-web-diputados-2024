4370e28dabd9f7fe5823fa1d60de42dc10c72e0e
Salino3 <flaviobcn012@gmail.com>
2024-09-28 11:16:12 +0200

    add firsts deputies

---
+++ src/core/data/deputies/deputies-data_01.tsx
@@ -0,0 +1,16 @@
+import { ValuesFilter } from "@/core/interfaces";
+
+// 1)
+export const arrayGrupoParlamentarioEHBildu_tag: ValuesFilter[] = [
+  { text: "Isabel Pozueta Fernández", value: "Isabel Pozueta Fernández" },
+  {
+    text: "Iñaki Ruiz de Pinedo Undiano",
+    value: "Iñaki Ruiz de Pinedo Undiano",
+  },
+  { text: "Jon Iñarritu García", value: "Jon Iñarritu García" },
+  { text: "Mertxe Aizpurua Arzallus", value: "Mertxe Aizpurua Arzallus" },
+  {
+    text: "Oskar Matute García de Jalón",
+    value: "Oskar Matute García de Jalón",
+  },
+];
--- src/core/data/index.ts
+++ src/core/data/index.ts
@@ -1,4 +1,5 @@
 export * from "./parlamentary-groups";
+export * from "./deputies";
 export * from "./comunidades-autonomicas";
 export * from "./provincias";
 export * from "./municipios";
