1febe8ff5c69f7c61eafdb83bc20dba87ec5eb2c
Salino3 <flaviobcn012@gmail.com>
2024-11-15 21:18:34 +0100

    add graphql to the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
+import i18next from "i18next";
 import { I18nextProvider } from "react-i18next";
+import App from "./App.tsx";
+import { ProviderApp } from "./core/provider-app.tsx";
 import global_es from "./i18next/es/global.json";
 import global_en from "./i18next/en/global.json";
 import global_it from "./i18next/it/global.json";
-import i18next from "i18next";
-import { ProviderApp } from "./core/provider-app.tsx";
 import "./index.scss";
+
+// Config Apollo client
+const client = new ApolloClient({
+  uri: `${process.env.VITE_APP_BASE_BACK_GRAPHQL}/graphql`,
+  cache: new InMemoryCache(),
+});
 
 const storedLanguage = sessionStorage.getItem("selectedLanguage") || "es";
 
@@ -32,9 +39,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
       <BrowserRouter>
-        <ProviderApp>
-          <App />
-        </ProviderApp>
+        <ApolloProvider client={client}>
+          <ProviderApp>
+            <App />
+          </ProviderApp>
+        </ApolloProvider>
       </BrowserRouter>
     </I18nextProvider>
   </React.StrictMode>
