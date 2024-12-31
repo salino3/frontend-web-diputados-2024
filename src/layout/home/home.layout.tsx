b5710d2ac3fd8f25dead512f9dc4b220fc889725
Salino3 <flaviobcn012@gmail.com>
2024-12-31 10:17:34 +0100

    move function fetchApi to home component

--- src/layout/home/home.layout.tsx
+++ src/layout/home/home.layout.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { GlobalContext, MyState } from "@/core";
 import { HeaderHome } from "@/common-app";
 import { HomePage } from "@/pods";
 import "./home.styles.scss";
 
 export const HomeLayout: React.FC = () => {
+  const { fetchApi } = React.useContext<MyState>(GlobalContext);
+
+  React.useEffect(() => {
+    fetchApi(1, 10);
+  }, []);
+
   return (
     <main className="rootHomeLayout">
       <HeaderHome />
