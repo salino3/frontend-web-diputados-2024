3588a7437bb85e61fa9b99ab9718cfb242ef0abc
Salino3 <flaviobcn012@gmail.com>
2024-12-31 10:37:43 +0100

    add arror icon

--- src/pods/error404/error404.component.tsx
+++ src/pods/error404/error404.component.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { appRoute } from "@/route";
 import "./error404.styles.scss";
 
 export const Error404: React.FC = () => {
   return (
     <div className="containerError404">
       <h1 className="titleError404">Error 404: Page Not Found</h1>
+      <Link className="routeError404" to={appRoute?.root}>
+        <img src="assets/icons/arrow-back.svg" alt="Arrow go back" />
+        <span>Go back</span>
+      </Link>
       <div className="boxImageError">
         <img src="assets/images/img_error_404.webp" alt="Image Error 404" />
       </div>
