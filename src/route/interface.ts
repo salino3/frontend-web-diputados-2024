620267f54f8a77068244de8ee28fcfbcd09cbe81
Salino3 <flaviobcn012@gmail.com>
2025-01-01 11:29:05 +0100

    changings interfaces

--- src/route/interface.ts
+++ src/route/interface.ts
@@ -1,9 +1,9 @@
-interface Route {
+interface Routes {
   root: string;
   error_404: string;
 }
 
-export const appRoute: Route = {
+export const appRoute: Routes = {
   root: "/frontend-web-diputados-2024",
   error_404: "/frontend-web-diputados-2024/*",
 };
